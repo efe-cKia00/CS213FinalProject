a869ec6a962fec8798d6eb92fd77f36e47262711
Jack Losing <jwlosing@gmail.com>
2025-12-10 12:16:25 -0800

    Merge efe to master
    * First implementation attempt of the homepage
    
    * Implemented SCRUM-11
    
    * Added SCRUM-33; User class
    
    * Updated SCRUM-33; User class
    
    * Added scaffolding for User model
    
    * Implemented SCRUM-14
    
    * Implemented SCRUM-14, 15, 34, and 35
    
    * Tim to Efe (and vice versa) (#2)
    
    * Added Service Model Class
    
    * Added Services Scafolding
    
    ---------
    
    Co-authored-by: Tim Middleton <middletontimothy53@gmail.com>
    
    * Implemented SCRUM 38 & 36. The Appointment, Service, and User Models have been created
    
    ---------
    
    Co-authored-by: Efe Awo-Osagie <deafveid@gmail.com>
    Co-authored-by: Jesse Dahlke <jesse.dahlke@ndsu.edu>
    Co-authored-by: Tim Middleton <middletontimothy53@gmail.com>

---
+++ Models/Service.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CS212FinalProject.Models
+{
+    public class Service
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public bool IsAvailable { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,49 @@
-using CS212FinalProject.Components;
+﻿using CS212FinalProject.Components;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CS212FinalProject.Data;
+using Shared.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<CS212FinalProjectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CS212FinalProjectContext") ?? throw new InvalidOperationException("Connection string 'CS212FinalProjectContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDbContextFactory<CS212FinalProjectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CS212FinalProjectContext") ?? throw new InvalidOperationException("Connection string 'CS212FinalProjectContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Add authentication for users as cookies
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
+
+// Add http context accessor
+builder.Services.AddHttpContextAccessor();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 
 var app = builder.Build();
+
+/**using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}**/
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -14,6 +51,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
